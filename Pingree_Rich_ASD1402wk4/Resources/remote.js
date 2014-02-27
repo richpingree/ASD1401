9e4b34a120057742aee131d620a0608493523d10
richpingree <richpingree@fullsail.edu>
2014-02-27 09:26:47 -0600

    added save function to remote.js file
    added save function to remote.js file

--- Pingree_Rich_ASD1402wk4/Resources/remote.js
+++ Pingree_Rich_ASD1402wk4/Resources/remote.js
@@ -1,3 +1,37 @@
+var saveOptions = {
+	title: "Save or Cancel",
+	options: ["Save", "Cancel"],
+	cancel: 1,
+	selectedIndex: 0
+};
+
+var saveWin = Ti.UI.createWindow({
+	title: "Save to Favorites",
+	backgroundColor: "blue",
+	layout: 'vertical'
+}); 
+
+var saveButton = Ti.UI.createButton({
+	title: "Save",
+	backgroundColor: "green",
+	color: "black",
+	borderRadius: 5,
+	top: 200,
+	height: 30,
+	width: 250,
+});
+
+var cancelButton = Ti.UI.createButton({
+	title: "Cancel",
+	backgroundColor: 'red',
+	color: "black",
+	borderRadius: 5,
+	height: 30,
+	width: 250
+});
+
+saveWin.add(saveButton, cancelButton);
+
 var sections = [];
 
 var remoteResponse = function(){
@@ -23,9 +57,39 @@ var remoteResponse = function(){
 			//hasChild: true
 		});
 		tableSecs.add(theRow);
+		
 	};
 	sections.push(tableSecs);
 	table.setData(sections);
+
+
+	table.addEventListener('click', function(){
+	var saveOpts = Ti.UI.createOptionDialog(saveOptions);
+	saveWin.open();
+	
+	
+	var saveFav = function(){
+		var favData = {};
+			favData.author = theRow.title.value;
+			favData.title =  theRow.state.value;
+			favData.score = theRow.score.value;
+
+		db.execute('INSERT INTO remotedb (author, title, score) VALUES (?,?,?)', favData.author, favData.title, favData.score);
+	
+		data = getRowData();
+		table1.setData(data);
+		
+		saveButton.removeEventListener('click', saveFav);
+		saveWin.close();
+	};
+	saveButton.addEventListener('click',saveFav);
+	
+	var cancelFav = function(){
+		cancelButton.removeEventlistener('click', cancelFav);
+		editWin.close();
+	};		 
+	cancelButton.addEventListener('click', cancelFav);
+});
 	
 //console calls for test
 	// Ti.API.debug(testTitle);
